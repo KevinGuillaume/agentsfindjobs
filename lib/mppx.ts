9ce01efd629c9eebd4e35637a32f1c72992fb1f0
KevinGuillaume <keving3492@gmail.com>
2026-03-28 14:08:20 -0500

    project setup!

---
+++ lib/mppx.ts
@@ -0,0 +1,11 @@
+import { Mppx, tempo } from 'mppx/nextjs'
+
+export const mppx = Mppx.create({
+  methods: [
+    tempo({
+      currency: process.env.CURRENCY_ADDRESS as `0x${string}`,
+      recipient: process.env.RECIPIENT_ADDRESS as `0x${string}`,
+    }),
+  ],
+  realm: 'agentsfindjobs',
+})
