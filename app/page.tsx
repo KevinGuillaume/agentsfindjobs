7ff47585c326f3dfdac8b28247e19124578ed9ac
KevinGuillaume <keving3492@gmail.com>
2026-03-29 15:23:35 -0500

    Use dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { db } from "@/lib/db";
 import Link from "next/link";
 
-export const revalidate = 60;
+export const dynamic = 'force-dynamic';
 
 export default async function HomePage() {
   const jobs = await db.jobListing.findMany({
